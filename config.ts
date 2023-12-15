2b5e960630e7dfe285298771201bd65674778891
matous <matous.trca@gmail.com>
2023-12-15 14:57:24 +0100

    Add pediatric practice description and hero image

--- config.ts
+++ config.ts
@@ -1,5 +1,14 @@
 export default {
   title: "Pediatrie Litochleby",
+  description: `
+    Vítáme vás na stránkách Pediatrie Litochleby MUDr. Kateřiny Rieger
+    Konopáskové nacházející se na Poliklinice Litochleby na pražském Chodově.
+    Jsme ordinace s dlouhodobou praxí, která byla založena v devadesátých letech
+    MUDr. Marcelou Machynkovou, jejíž pokračovatkou je atestované lékařka s
+    dlouholetouetou praxí v ambulantní a nemocniční pediatrii MUDr. Kateřina
+    Rieger Konopásková. Nabízíme komplexní péči o pacienty od narození až do
+    dospělosti,
+  `,
   links: [
     {
       title: "Náš tým",
