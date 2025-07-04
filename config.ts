d61159c767b2cee7f61141f4b2de6c09994f5ecc
Matouš Trča <matous.trca@gmail.com>
2025-07-04 16:54:43 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -10,7 +10,7 @@ export default {
     {
       text: `
         Letní dovolená <br>
-        30. 6. -  11. 7. a 11. 8. - 22. 8. <br>
+        30. 6. -  18. 7. a 11. 8. - 22. 8. <br>
         V době dovolené <i>akutní případy</i> řeší MUDr. Kohoutová, čekárna naproti. V případě zaslání receptu prosím o využití aplikace Medevio. <br> <br>
         Ordinační doba o prázdninách pro nemocné<br> po - pá 8-11 hod.
     `,
